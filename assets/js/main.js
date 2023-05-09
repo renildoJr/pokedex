dc89434c7a364bb881e28326d9b343c068c68015
renildoJr <juninho952020@gmail.com>
2023-05-08 21:55:10 -0300

    Criação da lógica do Modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const pokemonList = document.getElementById("pokemon-list")
 const loadMoreButton = document.querySelector("#loadMoreButton")
-const menuLinks = document.querySelectorAll(".nav-links  .menu li")
+const menuLinks = document.querySelectorAll(".nav-links .menu li")
 const detailsSections = document.querySelectorAll(".details-section")
 const maxRecords = 151
 const limit = 5
@@ -26,9 +26,73 @@ function loadPokemonItens(offset, limit) {
         pokemons = document.querySelectorAll(".pokemon")
 
         pokemons.forEach((pokemonLi, id)=>pokemonLi.addEventListener("click", ()=>{
-            pokeApi.getPokemon(id)
+            id+=1
+            openModal(id)
         }))
    })
+}
+
+function openModal(id) {
+    const pokemonModal = document.querySelector(".modal-container")
+
+    pokemonModal.classList.remove("disable")
+    pokemonList.parentElement.classList.add("disable")
+
+    function exitModal () {
+        pokemonModal.classList.add("disable")
+        pokemonList.parentElement.classList.remove("disable")
+    }
+
+    pokeApi.getPokemon(id).then(pokemon => pokemonModal.innerHTML = `
+        <section class="modal ${pokemon.type}">
+            <header class="pokemon-header">
+                <a class="btn" id="btn-return"><i class="fa-solid fa-arrow-left"></i></a>
+                <h1 class="name">${pokemon.name}</h1>
+                <span class="number">${pokemon.number}</span>
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img class="pokemon-img" src="${pokemon.photo}" alt="${pokemon.name}">
+            </header>
+            <main>
+                <nav class="nav-links">
+                    <ul class="menu">
+                        <li class="selected">About</li>
+                        <li>Base Stats</li>
+                        <li>Evolution</li>
+                        <li>Moves</li>
+                    </ul>
+                </nav>
+                <section class="details-section active">
+                    <ul>
+                        <li>Species <strong>Seed</strong></li>
+                        <li>Height <strong>${pokemon.height}</strong></li>
+                        <li>Weight <strong>${pokemon.weight}</strong></li>
+                        <li>Avilities <strong>Overgrow, Chlorophyl</strong></li>
+                        <li><h2>Breeding</h2></li>
+                        <li>Gender <strong>(icon) 87.5%  (icon) 12.5%</strong></li>
+                        <li>Egg Groups <strong>Monster</strong></li>
+                        <li>Egg Cycle <strong>Grass</strong></li>
+                    </ul>
+                    <span>Types defenses</span>
+                    <p>The effectiveness of each type on Charmander</p>
+                </section>
+                <section class="details-section">
+                    <ul>
+                        <li>HP <strong>45 (bar)</strong></li>
+                        <li>Attack <strong>45 (bar)</strong></li>
+                        <li>Defense <strong>45 (bar)</strong></li>
+                        <li>Sp. Atk <strong>45 (bar)</strong></li>
+                        <li>Sp. Def <strong>45 (bar)</strong></li>
+                        <li>Speed <strong>45 (bar)</strong></li>
+                        <li>Total <strong>45 (bar)</strong></li>
+                    </ul>
+                </section>
+            </main>
+        </section>
+    
+    `)
+    
 }
 
 loadMoreButton.addEventListener('click', ()=>{
@@ -47,12 +111,18 @@ loadMoreButton.addEventListener('click', ()=>{
 
 })
 
-menuLinks.forEach((link, i, linksArr)=>link.addEventListener("click", ()=>{
-    detailsSections.forEach(detailsSection=>detailsSection.classList.remove("active"))
-    linksArr.forEach(link=>link.classList.remove("selected"))
-    link.classList.add("selected")
-    detailsSections[i].classList.add("active")
-}))
+function loadFunctions() {
+
+    
+
+    menuLinks.forEach((link, i, linksArr)=>link.addEventListener("click", ()=>{
+        detailsSections.forEach(detailsSection=>detailsSection.classList.remove("active"))
+        linksArr.forEach(link=>link.classList.remove("selected"))
+        link.classList.add("selected")
+        detailsSections[i].classList.add("active")
+    }))
+}
+
 
 loadPokemonItens(offset, limit)
 
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -4,24 +4,26 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
-    
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
-    
     pokemon.types = types
     pokemon.type = type
-
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    pokemon.height = pokeDetail.height
+    pokemon.weight = pokeDetail.weight
+    
+    
+
 
     return pokemon
 
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
+
     return fetch(pokemon.url)
             .then(response => response.json())
             .then(convertPokeApiDetailToPokemon)
-
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
@@ -33,10 +35,9 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then(pokemonsDetails => Promise.all(pokemonsDetails))
 }
 
-// pokeApi.getPokemon = (pokemonNumber) => {
-//     const url = `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`
-//     return fetch(url)
-//         .then(response => response.json())
-//         .then(co)
-        
-// } 
+pokeApi.getPokemon = (id) => {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+                .then(response => response.json())
+                .then(convertPokeApiDetailToPokemon)
+                .then(pokemon => pokemon)
+}
